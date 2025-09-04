99b4d575c2a58bb34ba0fb1d5571e20c1a57a005
pauloguarino <pauloguarino97@gmail.com>
2025-09-04 16:21:58 -0300

    Programmed basic blink program.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,26 @@
 #include <Arduino.h>
 
+#define LED 2
+#define T 500
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+
+  pinMode(LED, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(LED, HIGH);
+  Serial.println("High");
+  delay(T);
+  digitalWrite(LED, LOW);
+  Serial.println("Low");
+  delay(T);
 }
 
 // put function definitions here:
