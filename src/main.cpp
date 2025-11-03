36f55e30cff71f2fd7f3c921a327912245694693
Paulo Guarino <pauloguarino97@gmail.com>
2025-11-03 01:29:00 -0300

    Wrote ISR example.

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,71 @@
 #include <Arduino.h>
 
-#define LED 2
-#define T 500
+#define ESP32_LED 2
+#define YELLOW_LED 23
+#define BUTTON 36
+#define MS 1000
+#define ESP32_LED_T 2*MS
+#define YELLOW_LED_T 500*MS
+#define DEBOUNCE_TIME 250
+#define PRESCALER_1MHZ 80
+#define SERIAL_MONITOR_BAUDRATE 115200
+
 // put function declarations here:
-int myFunction(int, int);
+void toggle_led_isr();
+void timer_isr();
 
 float i = 0;
+volatile bool led_state = false;
+volatile unsigned long button_press_time = 0;
+volatile unsigned long last_button_press_time = 0;
+hw_timer_t *yellow_led_timer = NULL;
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-
-  pinMode(LED, OUTPUT);
-  Serial.begin(115200);
+  pinMode(ESP32_LED, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);
+  pinMode(BUTTON, INPUT);
+  Serial.begin(SERIAL_MONITOR_BAUDRATE);
+  attachInterrupt(BUTTON, toggle_led_isr, FALLING);
+  yellow_led_timer = timerBegin(0, PRESCALER_1MHZ, true); // número de timer, prescaler (80 MHz), ascendente o descendente
+  timerAttachInterrupt(yellow_led_timer, timer_isr, true);
+  timerAlarmWrite(yellow_led_timer, 500*MS, true); // timer, final del contador, reload
+  timerAlarmEnable(yellow_led_timer);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  i += 0.1;
-  digitalWrite(LED, HIGH);
-  Serial.println("High");
-  delay(T);
-  digitalWrite(LED, LOW);
-  Serial.println("Low");
-  delay(T);
-  Serial.print(">sin:");
-  Serial.println(sin(i));
+  if(led_state){
+    led_state = false;
+    digitalWrite(ESP32_LED, LOW);
+    Serial.println("Low");
+  }else{
+    led_state = true;
+    digitalWrite(ESP32_LED, HIGH);
+    Serial.println("High");
+  }
+  delay(ESP32_LED_T);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void IRAM_ATTR toggle_led_isr(){
+  button_press_time = millis();
+  if(button_press_time - last_button_press_time > DEBOUNCE_TIME){
+    if(timerAlarmEnabled(yellow_led_timer)){
+      timerAlarmDisable(yellow_led_timer);
+    }else{
+      timerAlarmEnable(yellow_led_timer);
+    }
+    if(led_state){
+      led_state = false;
+      digitalWrite(ESP32_LED, LOW);
+    }else{
+      led_state = true;
+      digitalWrite(ESP32_LED, HIGH);
+    }
+    last_button_press_time = button_press_time;
+  }
+}
+
+void IRAM_ATTR timer_isr(){
+  digitalWrite(YELLOW_LED, !digitalRead(YELLOW_LED));
 }
